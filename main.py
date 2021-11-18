d1f61d128c6f8ddde74291f953b263e010d5000e
Mart Kaasik <mart.kaasik@gmail.com>
2021-11-18 12:08:55 +0200

    main

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Game:
             self.render()
             self.clock.tick(60)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     game = Game()
     game.run()
     pygame.quit()
