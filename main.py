7f2b7fd5e4f029e273d0bde959b4f427bf1a168c
Kristjan Kõiv <kristjankv32@gmail.com>
2022-02-28 12:34:13 +0200

    Added class and object.:

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ def add(a, b):
     return a + b
 
 
+class Employee:
+    def __init__(self, name, address, salary):
+        self.name = name
+        self.address = address
+        self.salary = salary
+
+
 if __name__ == '__main__':
+    # Function
     c = add(2, 2)
     print(c)
+
+    # Class / object
+    employee1 = Employee("John", "Tallinn", 500)  # employee1 is an object of class Employee
+    print(employee1.address)
+    employee1.address = "Tartu"
+    print(employee1.address)
